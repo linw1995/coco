7cac69eb46c1b39d77e00c006dabb888e613bbe8
林玮 (Jade Lin) <linw1995@icloud.com>
2026-03-24 13:05:55 +0800

    Initial commit
    Add coco-mem memory management library with pre-commit hooks

---
+++ README.md
@@ -0,0 +1,3 @@
+# CoCo
+
+An AI Copilot.
---
+++ flake.nix
@@ -0,0 +1,54 @@
+rec {
+  description = "CoCo - An AI Copilot";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    crane,
+    ...
+  } @ inputs:
+    utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.fenix.overlays.default
+          ];
+        };
+        lib = pkgs.lib;
+        version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).workspace.package.version;
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs.fenix; [
+              (combine [
+                stable.cargo
+                stable.clippy
+                stable.rust-src
+                stable.rustc
+                stable.rustfmt
+                latest.rust-analyzer
+                stable.llvm-tools
+              ])
+            ];
+            packages = with pkgs; [
+              prek
+              grcov
+
+              cargo-nextest
+            ];
+          };
+        };
+      }
+    );
+}
