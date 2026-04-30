9a81e86c8c36b5f0f4bf2f4cfc5bcb12228e226d
林玮 (Jade Lin) <linw1995@icloud.com>
2026-04-30 16:48:27 +0800

    Add coco-channel message crate

--- coco-core/src/lib.rs
+++ coco-core/src/lib.rs
@@ -5,13 +5,13 @@ mod resolver;
 mod service;
 mod skill;
 
+pub use coco_channel::{ChannelKind, InboundMessage, OutboundMessage};
 pub use coco_llm::coco_mem::JobStatus;
 pub use engine::{ConversationEngine, JobStatusSnapshot};
 pub use error::{BranchResolveError, EngineError, Error};
 pub use message::{
     BatchPromptRequest, BatchPromptResult, BranchPromptFailure, BranchPromptOutcome,
-    BranchPromptRequest, BranchPromptResult, BranchPromptStatus, BranchPromptSuccess, ChannelKind,
-    InboundMessage, OutboundMessage,
+    BranchPromptRequest, BranchPromptResult, BranchPromptStatus, BranchPromptSuccess,
 };
 pub use resolver::{BranchResolver, FixedBranchResolver};
 pub use service::CoreService;
